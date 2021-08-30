8b116e1338819da73df091cf55040f105cdbc0c0
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-08-30 15:18:39 +0200

    docs: configure Docusaurus

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -1,88 +1,35 @@
 // See https://docusaurus.io/docs/site-config for all the possible
 // site configuration options.
 
-const siteConfig = {
-  title: "RxJS Fundamentals", // Title for your website.
-  tagline: "An Open Learning course by This is Learning",
-  url: "https://your-docusaurus-test-site.com", // Your website URL
-  baseUrl: "/", // Base URL for your project */
-  // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
+const organizationName = "this-is-learning";
+const projectName = "rxjs-fundamentals-course";
 
-  // Used for publishing and more
-  projectName: "test-site",
-  organizationName: "facebook",
-  // For top-level user or org sites, the organization is still the same.
-  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
-  //   organizationName: 'JoelMarcey'
-
-  // For no header links in the top nav bar -> headerLinks: [],
-  headerLinks: [{ doc: "part-1", label: "Course" }],
-
-  // If you have users set above, you add it here:
-  // users,
-
-  /* path to images for header/footer */
-  headerIcon: "img/favicon.ico",
-  footerIcon: "img/favicon.ico",
-  favicon: "img/favicon.ico",
-
-  /* Colors for website */
+module.exports = {
+  baseUrl: `/${projectName}/`,
+  cleanUrl: true,
   colors: {
     primaryColor: "#906331",
     secondaryColor: "#644522",
   },
-
-  /* Custom fonts for website */
-  /*
-  fonts: {
-    myFont: [
-      "Times New Roman",
-      "Serif"
-    ],
-    myOtherFont: [
-      "-apple-system",
-      "system-ui"
-    ]
-  },
-  */
-
-  // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
   copyright: "Licensed under CC BY-SA 4.0 by This is Learning",
-
+  favicon: "img/favicon.ico",
+  footerIcon: "img/favicon.ico",
+  headerIcon: "img/favicon.ico",
+  headerLinks: [{ doc: "part-1", label: "Course" }],
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
     theme: "default",
   },
-
-  // Add custom scripts here that would be placed in <script> tags.
-  scripts: ["https://buttons.github.io/buttons.js"],
-
-  // On page navigation for the current documentation page.
-  onPageNav: "separate",
-  // No .html extensions for paths.
-  cleanUrl: true,
-
-  // Open Graph and Twitter card images.
   ogImage: "img/undraw_online.svg",
+  onBrokenLinks: "error",
+  onBrokenMarkdownLinks: "warn",
+  onPageNav: "separate",
+  organizationName,
+  projectName,
+  scripts: ["https://buttons.github.io/buttons.js"],
+  tagline: "An Open Learning course by This is Learning",
+  title: "RxJS Fundamentals",
   twitterImage: "img/undraw_tweetstorm.svg",
-
   twitterUsername: "Thisis_Learning",
-
-  // For sites with a sizable amount of content, set collapsible to true.
-  // Expand/collapse the links and subcategories under categories.
-  // docsSideNavCollapsible: true,
-
-  // Show documentation's last contributor's name.
-  // enableUpdateBy: true,
-
-  // Show documentation's last update time.
-  // enableUpdateTime: true,
-
-  // You may provide arbitrary config keys to be used as needed by your
-  // template. For example, if you need your repo's URL...
-  // repoUrl: 'https://github.com/facebook/test-site',
+  url: `https://${organizationName}.github.io`,
 };
-
-module.exports = siteConfig;
